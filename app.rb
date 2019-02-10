9ca972aa0bc03c51f81e3cc2589e51b2a3253c96
Ray Mannion <g00340315@gmit.ie>
2019-02-10 19:37:36 +0000

    fixing number and empty space error

--- app.rb
+++ app.rb
@@ -44,11 +44,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if(!@game.guess(letter))
-		flash[:message]="You have already used that letter"
-	end
-	redirect '/show'
-
+    begin
+      if(!@game.guess(letter))
+  	    flash[:message]="You have already used that letter"
+      end
+	  rescue ArgumentError =>_
+	  
+	    flash[:message]="You must enter a letter"
+	  end
+	    redirect '/show'
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
